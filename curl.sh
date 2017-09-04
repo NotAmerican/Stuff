df90b039a1b60284cb78a0b61457a34005671c43
Mike McDonald <mike90@gmail.com>
2017-09-04 10:06:50 -0400

    rawr

--- curl.sh
+++ curl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-response=$( curl -s -o /dev/null -w "%{http_code}" http://wvnet.edu/)
+response=$( curl -s -o /dev/null -w "%{http_code}" http://google.com/)
 if [ $response != "302" ]; then
-	mail -s "Script Test" @email.uscc.net <<< "SteelTown is broken again"
+	mail -s "Script Test" mike@rtswv.com <<< "Google has returned an inproper status code."
 fi
