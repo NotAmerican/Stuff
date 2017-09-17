5ced7e6cc69307458f434a3ee93956c6afe79b3d
Mike McDonald <mike@rtswv.com>
2017-09-17 08:30:55 -0400

    different

--- dumper.sh
+++ dumper.sh
@@ -9,7 +9,7 @@ mv ~/backups/* ~/backups/archive/ > /dev/null 2>&1
 DBSERVER=odin.rtswv.com
 USER=$1
 PASS=$2
-declare -a databases=('finalmile' 'infinitewp' 'masonrid_user' 'mjgrill' 'mjsaloon' 'nagios' 'short' 'steel' 'warriors_user' 'westover_user' 'wordpress' 'wp_band')
+databases=('finalmile' 'infinitewp' 'masonrid_user' 'mjgrill' 'mjsaloon' 'nagios' 'short' 'steel' 'warriors_user' 'westover_user' 'wordpress' 'wp_band')
 for database in ${databases[@]}
 do
    FILE=~/backups/${database}_backup.sql.`date +"%Y%m%d"`
